d9396cf9b14a05c2ad106ef35a85f10ce90cdaad
Amar Arbat <amararbat03@gmail.com>
2024-03-07 08:47:57 +0530

    Animated ToolTip and wavy background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -16,6 +17,7 @@ export default function Home() {
        <WhyChooseUs/>
        <MusicSchoolTestimonials/>
        <UpcomingWebinars/>
+       <Instructors/>
     </main>
   );
 }
